5d0ad28c9edf02b84e5c40960fb6024e9bf0bdbf
alexis <alexis.yalle.27@unsch.edu.pe>
2021-05-31 16:43:01 -0500

    actualizado script sql-server a mysql

--- src/main/java/com/ecoedu/Vistas/Herramienta.java
+++ src/main/java/com/ecoedu/Vistas/Herramienta.java
@@ -200,11 +200,16 @@ public class Herramienta {
         List<T> listGenericos=new ArrayList<>();
         try {
             Query query=jpa.createQuery("SELECT p FROM "+generico.getSimpleName()+" p where "+Columna+" BETWEEN "+"'"+
-            (FechaInicio.getYear()+1900)+(Herramienta.conOsin0(FechaInicio.getMonth()+1))+(Herramienta.conOsin0(FechaInicio.getDate())+" 00:00:00 ")+"'"
+            (FechaInicio.getYear()+1900)+"-"+(Herramienta.conOsin0(FechaInicio.getMonth()+1))+"-"+(Herramienta.conOsin0(FechaInicio.getDate())+" 00:00:00 ")+"'"
             +" and "+"'"+
-            (FechaFin.getYear()+1900)+(Herramienta.conOsin0(FechaFin.getMonth()+1))+(Herramienta.conOsin0(FechaFin.getDate()))+" 23:59:59 "+"'"
+            (FechaFin.getYear()+1900)+"-"+(Herramienta.conOsin0(FechaFin.getMonth()+1))+"-"+(Herramienta.conOsin0(FechaFin.getDate()))+" 23:59:59 "+"'"
             +" and id_Control_paciente = "+idControl);
         listGenericos=query.getResultList();  
+            System.out.println("SELECT p FROM "+generico.getSimpleName()+" p where "+Columna+" BETWEEN "+"'"+
+            (FechaInicio.getYear()+1900)+"-"+(Herramienta.conOsin0(FechaInicio.getMonth()+1))+"-"+(Herramienta.conOsin0(FechaInicio.getDate())+" 00:00:00 ")+"'"
+            +" and "+"'"+
+            (FechaFin.getYear()+1900)+"-"+(Herramienta.conOsin0(FechaFin.getMonth()+1))+"-"+(Herramienta.conOsin0(FechaFin.getDate()))+" 23:59:59 "+"'"
+            +" and id_Control_paciente = "+idControl);
             
         } catch (Exception e) {
             System.out.println(e.toString()+" HUBO ERROR DE CONSULTA");
